ec2400ad76e6c9dd72d4ae4c8cb64c2b6ed34d85
Tristan <yourname@example.com>
2023-12-22 00:54:46 -0800

    Initial Commit

---
+++ Memorize/MemorizeApp.swift
@@ -0,0 +1,17 @@
+//
+//  MemorizeApp.swift
+//  Memorize
+//
+//  Created by Tristan Khieu on 12/22/23.
+//
+
+import SwiftUI
+
+@main
+struct MemorizeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
